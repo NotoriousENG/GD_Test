ac0e03f4dc3cf1e8057f10f943187db82402a832
NotoriousENG <michaeljoconnell@ufl.edu>
2019-07-25 19:47:46 -0400

    up is negative lmao

--- src/StateMachine.gd
+++ src/StateMachine.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 const GRAVITY = 200.0
 const WALK_ACCEL = 2000
-const JUMP_VELOCITY = 20
+const JUMP_VELOCITY = -200
 const FRICTION = 10000
 const WALK_INITIAL_VELOCITY = 80
 const WALK_MAX_VELOCITY = 500
@@ -20,6 +20,7 @@ var state_ = State.IDLE
 
 
 func _physics_process(delta):
+	print(state_)
 	match [state_]:
 		[State.IDLE]:
 			if (velocity.x > 0):
@@ -50,13 +51,18 @@ func _physics_process(delta):
 			state_ = State.FALLING
 		[State.FALLING]:
 			velocity.y += delta * GRAVITY
+			if (velocity.x > 0):
+				velocity.x = max(0, velocity.x - delta * FRICTION/5)
+			elif (velocity.x < 0):
+				velocity.x = min(0, velocity.x + delta * FRICTION/5)
+				
 			if (Input.is_action_pressed("ui_left")):
 				velocity.x = min(velocity.x, -WALK_INITIAL_VELOCITY)
 				velocity.x = max(velocity.x + delta * -WALK_ACCEL, -WALK_MAX_VELOCITY)
 			elif (Input.is_action_pressed("ui_right")):
 				velocity.x = max(velocity.x, WALK_INITIAL_VELOCITY)
 				velocity.x = min(velocity.x + delta * WALK_ACCEL, WALK_MAX_VELOCITY)
-			if (is_on_floor()):
+			if (is_on_floor() and velocity.y > 0 ):
 				velocity.y = 0
 				state_ = State.IDLE
 	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
